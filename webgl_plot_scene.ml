b16f3ce9b3fa1e5abcb1c6e312bc1633a627e466
Marc Lasson <marc.lasson@lexifi.com>
2016-10-20 08:56:25 +0200

    Hide cursor only when too close from the ball

--- webgl_plot_scene.ml
+++ webgl_plot_scene.ml
@@ -296,7 +296,6 @@ let prepare_scene gl component =
               component # set_cursor_visibility true;
             end
           | (_, p, obj) :: _ -> begin
-              component # set_cursor_visibility false;
               pointer_projection <- Vector.to_three p;
               pointer_magnetic <- obj # magnetize pointer_projection;
               let (x,y,z) = pointer_magnetic in
@@ -305,6 +304,8 @@ let prepare_scene gl component =
                 multiply_vector matrix (Vector.of_four (x,y,z, 1.0))
               in
               let x,y, _ = Vector.to_three (Vector.four_to_three q) in
+              let x',y' = pointer in
+              component # set_cursor_visibility (Math.sq (x -. x') +. Math.sq (y -. y') > 0.001);
               textbox # set_position (x,y);
               selected_object <- Some obj;
             end
